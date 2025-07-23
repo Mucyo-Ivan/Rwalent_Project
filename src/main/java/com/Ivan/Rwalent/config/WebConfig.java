a34102c7bb433ca0604cdaef38149c38b72471d5
Mucyo-Ivan <mucyoivan25@gmail.com>
2025-07-23 10:25:42 +0200

    commmit

--- src/main/java/com/Ivan/Rwalent/config/WebConfig.java
+++ src/main/java/com/Ivan/Rwalent/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.Ivan.Rwalent.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import org.springframework.context.annotation.Bean;
 
@@ -11,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public HandlerMappingIntrospector handlerMappingIntrospector() {
         return new HandlerMappingIntrospector();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://rwalent-front-end.vercel.app/", "http://localhost:8081") // Replace with your actual Vercel URL
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 } 
